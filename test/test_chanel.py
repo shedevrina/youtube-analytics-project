6e23ec4172e4dc68bae96be21416d3e37e2dd57d
shedevrina <shevrina88@gmail.com>
2023-06-27 00:42:52 +0700

    homework-1

--- test/test_chanel.py
+++ test/test_chanel.py
@@ -1,4 +1,12 @@
 import pytest
+
 from srs.channel import Channel
 
+
 @pytest.fixture
+def Channel1():
+    return Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+
+def test_channel_print_info(Channel1):
+    assert isinstance(youtube.channels().list(id=Channel1, part='snippet,statistics').execute(), list)
+
